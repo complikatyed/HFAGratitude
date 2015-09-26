b4c9d07d2f4dc38784e710e80197257148024e8d
Katye Russell <katye.russell@gmail.com>
2015-09-26 17:21:55 -0500

    Add routes and initial views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'landing#index'
+
+  get 'home' => 'home#index', as: 'home'
+  get 'landing/index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
